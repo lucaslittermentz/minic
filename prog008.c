9b281d9267222703c4cca3106175fb8b12ac6f34
lucaslittermentz <lucaslittermentz@users.noreply.github.com>
2016-05-03 20:17:55 -0300

    Create prog008.c

---
+++ prog008.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, max, min, i;
+    scanf("%d",&n);
+    int a[n];
+    for (i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    }
+    min = max = a[0];
+    for (i=0;i<n;i++) {
+        if (a[i]>max) max=a[i];
+        if (a[i]<min) min=a[i];
+    }
+    printf("\nMax: %d \nMin: %d\n",max,min);
+
+    return 0;
+}
