2c42bd851cfbd4250c806c49848bcfcea18d4f83
lucaslittermentz <lucaslittermentz@users.noreply.github.com>
2016-05-03 20:30:51 -0300

    Update prog008.c

--- prog008.c
+++ prog008.c
@@ -3,16 +3,18 @@
 
 int main()
 {
-    int n, max, min, i;
+    int n, pos, min, i;
     scanf("%d",&n);
     int a[n];
     for (i=0;i<n;i++) {
         scanf("%d",&a[i]);
     }
-    min = max = a[0];
+    min = a[0];
     for (i=0;i<n;i++) {
-        if (a[i]>max) max=a[i];
-        if (a[i]<min) min=a[i];
+        if (a[i]<=min) {
+            min=a[i];
+            pos=i;
+        }
     }
     printf("\nMax: %d \nMin: %d\n",max,min);
 
